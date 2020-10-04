5dfc8380708d6df6b6b436e87740373363e0b635
nazyie <engkunazri0609@gmail.com>
2020-10-04 16:14:18 +0800

    basic error handling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+  <h2>{{ $exception->getMessage() }}</h2>
+</body>
+
+</html>
+
+<!-- 
+
+To publish the configuration 
+
+php artisan vendor:publish -tag=laravel-errors
+-->
+
+<body>
